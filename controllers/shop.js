a80701268dfc4db811e1259716a3e87ccfbc9e16
erikqb3 <erikqb3@gmail.com>
2022-02-10 22:31:05 -0700

    Finished "Clearing the cart After Storing an order

--- controllers/shop.js
+++ controllers/shop.js
@@ -99,6 +99,9 @@ exports.postOrder = (req, res, next) => {
       return order.save()
     })
     .then(result => {
+      return req.user.clearCart();
+    })
+    .then(() => {
       res.redirect('/orders');
     })
     .catch(err => console.log(err));
--- models/user.js
+++ models/user.js
@@ -56,6 +56,11 @@ userSchema.methods.removeFromCart = function(productId) {
   return this.save();
 }
 
+userSchema.methods.clearCart = function() {
+  this.cart = { items: [] };
+  return this.save();
+}
+
 module.exports = mongoose.model('User', userSchema); //mongoose makes names database in plural form
 
 
