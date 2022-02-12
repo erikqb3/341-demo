b7d18d23f8e00395eb9360197e19d14ff84a7edc
erikqb3 <erikqb3@gmail.com>
2022-02-12 10:20:12 -0700

    Finished "Adding Additoinal Flass Messages"

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,17 +3,30 @@ const bcrypt = require('bcryptjs'); //npm install --save bcryptjs
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    errorMessage: req.flash('error')
+    errorMessage: message
   });
 };
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
+    errormessage: message
   });
 };
 
@@ -37,6 +50,7 @@ exports.postLogin = (req, res, next) => {
               res.redirect('/');
             });
           };
+          req.flash('error', 'Invalid email or password')
           res.redirect('/login');
         })
         .catch(err => {
@@ -54,6 +68,7 @@ exports.postSignup = (req, res, next) => {
   User.findOne({email: email}) //left-> database email, right->input email
     .then(userDoc => {
       if (userDoc) {
+        req.flash('error', 'Email exists already, please pick a different one.')
         return res.redirect('/signup)');
       }
       return bcrypt.hash(password, 12) //12 -> salt value, how much hashing is done, 12 is standard highest
