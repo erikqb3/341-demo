c596fc847caa4fca9293faa337175de5a954b80a
erikqb3 <erikqb3@gmail.com>
2022-02-12 12:50:10 -0700

    Finshed "Why we need Authorization"

--- routes/auth.js
+++ routes/auth.js
@@ -20,6 +20,6 @@ router.post('/reset', authController.postReset);
 
 router.get('/reset/:token', authController.getNewPassword);
 
-router.poset('/new-password', authController.postNewPassword);
+router.post('/new-password', authController.postNewPassword);
 
 module.exports = router;
