a84e3fb0a3171971a24dc9ac8ca649294a81e01f
erikqb3 <erikqb3@gmail.com>
2022-02-17 13:35:44 -0700

    Fixing some stuff

--- controllers/error.js
+++ controllers/error.js
@@ -1,15 +1,15 @@
 exports.get404 = (req, res, next) => {
-  res.status(404).render('404', { 
-    pageTitle: 'Page Not Found', 
+  res.status(404).render('404', {
+    pageTitle: 'Page Not Found',
     path: '/404',
-    isAuthenticated: req.session.isLoggedIn
+    isAuthenticated: req.session.isLoggedIn,
   });
 };
 
 exports.get500 = (req, res, next) => {
-  res.status(500).render('500', { 
-    pageTitle: '⚠ Error! Error! Error! ⚠', 
+  res.status(500).render('500', {
+    pageTitle: '⚠ Error! Error! Error! ⚠',
     path: '/500',
-    isAuthenticated: req.session.isLoggedIn
+    isAuthenticated: req.session.isLoggedIn,
   });
 };
